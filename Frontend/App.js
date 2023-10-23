c8bd034f79e54527d75e2332dd18d8620c2a9f3c
ThomasPollicino <122580685+ThomasPollicino@users.noreply.github.com>
2023-10-23 13:41:18 -0700

    com
    commit oct 23 1:41

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './StackNavigator'; 
+import { createStackNavigator } from '@react-navigation/stack';
 
-import CreateAccountScreen from './CreateAccountScreen';
-import LoginScreen from './LoginScreen';
+import CreateAccountScreen from './screens/CreateAccountScreen';
+import LoginScreen from './screens/LoginScreen';
+import CreateListing from './CreateListing';
 
 const Stack = createStackNavigator();
 
@@ -11,12 +13,13 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Navigator initialRouteName="CreateListing">
+        <Stack.Screen name="Login" component={LoginScreen}options={{ headerShown: false}} />
         <Stack.Screen name="CreateAccount" component={CreateAccountScreen} />
+        <Stack.Screen name="CreateListing" component={CreateListing} options={{ headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-export default App;
+//export default App;
--- Frontend/CreateListing.js
+++ Frontend/CreateListing.js
@@ -1,55 +1,196 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Alert,
+} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 const CreateListing = () => {
-  const [title, setTitle] = useState(''); //title variable
-  const [description, setDescription] = useState(''); //description variable
-  const [price, setPrice] = useState(''); //price variable 
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [photos, setPhotos] = useState([]);
 
-  const handleCreateListing = () => {
-    // Code to create a listing with database
+  const convertBlobToBase64 = (blob) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        resolve(reader.result);
+      };
+      reader.onerror = (error) => {
+        reject(error);
+      };
+      reader.readAsDataURL(blob);
+    });
   };
 
-  const handleUploadPhoto = () => {
-    // Add code for handling photo uploads here
+  const handleCreateListing = async () => {
+    try {
+      const photoDataPromises = photos.map(async (photo) => {
+        const base64Data = await convertBlobToBase64(photo.blob);
+        return base64Data;
+      });
+
+      const base64Images = await Promise.all(photoDataPromises);
+
+      const requestPayload = {
+        price: price,
+        
+        title: title,
+        description: description,
+        username: 'sampleUser',
+      };
+
+      const formData = new FormData();
+      photos.forEach((item, i) => {
+        formData.append("media_files[]", {
+          uri: item.uri,
+          type: "image/jpeg",
+          name: item.filename || `filename${i}.jpg`,
+        });
+      });
+      
+      
+       
+      
+      
+
+      console.log('request Payload:', requestPayload);
+
+      const response = await fetch('http://blitzbuyr.lol/api/createListing', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestPayload),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(`Response: ${JSON.stringify(data)}`);
+      } else {
+        console.error(`API Response Error: ${response.status}`);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleUploadPhoto = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsMultipleSelection: true,
+    });
+
+    if (!result.cancelled) {
+      const selectedPhotos = result.assets;
+      
+      // Fetch and convert selected photos to blobs
+      // Create an array of promises for fetching and converting the selected photos to blobs
+      const photoDataPromises = selectedPhotos.map(async (photo) => {
+        const response = await fetch(photo.uri);
+        const blob = await response.blob();
+        return blob;
+      });
+  
+      // Wait for all fetch and conversion operations to complete
+      const photoData = await Promise.all(photoDataPromises);
+  
+      // Add selected photos and their blobs to the 'photos' array
+      const updatedPhotos = photos.concat(selectedPhotos.map((photo, index) => ({
+        ...photo,
+        blob: photoData[index],
+      })));
+      setPhotos(updatedPhotos);
+    }
+  };
+
+  const handleDeletePhoto = index => {
+    Alert.alert('Delete Photo', 'Are you sure you want to delete this photo?', [
+      {
+        text: 'Cancel',
+        style: 'cancel',
+      },
+      {
+        text: 'Delete',
+        onPress: () => {
+          const updatedPhotos = [...photos];
+          updatedPhotos.splice(index, 1);
+          setPhotos(updatedPhotos);
+        },
+      },
+    ]);
   };
 
   return (
-    <View style={styles.container}> 
-      <Text style={styles.title}>Create a Listing</Text> 
+    <View style={styles.container}>
+      <Text style={styles.title}>Create a Listing</Text>
       <TextInput
         style={styles.input}
         placeholder="Title"
         value={title}
-        onChangeText={text => setTitle(text)}
+        onChangeText={(text) => setTitle(text)}
+        returnKeyType="done" // This allows users to close the keyboard
       />
       <TextInput
         style={[styles.input, styles.multilineInput]}
         placeholder="Description"
         value={description}
-        onChangeText={text => setDescription(text)}
-        multiline
+        onChangeText={(text) => setDescription(text)}
+        multiline={true}
+        textAlignVertical="top" // Allows users to return to a new line
       />
       <TextInput
         style={styles.input}
         placeholder="Price"
         value={price}
-        onChangeText={text => setPrice(text)}
+        onChangeText={(text) => setPrice(text)}
         keyboardType="numeric"
+        returnKeyType="done" // This allows users to close the numeric keyboard
       />
       <TouchableOpacity style={styles.uploadButton} onPress={handleUploadPhoto}>
-        <Image source={require('./assets/ImageIcon.png')} style={styles.uploadIcon} />
+        <Image
+          source={require('./assets/ImageIcon.png')}
+          style={styles.uploadIcon}
+        />
       </TouchableOpacity>
-      <Button title="Create Listing" onPress={handleCreateListing} />
+
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {photos.map((photo, index) => (
+          <View key={index}>
+            <Image source={{uri: photo.uri}} style={styles.uploadedPhoto} />
+            <TouchableOpacity
+              style={styles.deleteButton}
+              onPress={() => handleDeletePhoto(index)}>
+              <Text style={styles.deleteButtonText}>Delete</Text>
+            </TouchableOpacity>
+          </View>
+        ))}
+      </ScrollView>
+
+      <Button
+        title="Create Listing"
+        onPress={handleCreateListing}
+        style={styles.createListingButton}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  // Your existing styles here'
+
   container: {
     flex: 1,
-    paddingTop: 50,//top of page
-    padding: 30,//all around
+    paddingTop: 50, //top of page
+    padding: 30, //all around
     backgroundColor: '#fff',
   },
   title: {
@@ -70,7 +211,7 @@ const styles = StyleSheet.create({
   uploadButton: {
     backgroundColor: 'white',
     width: '100%',
-    height: 300,
+    height: 250,
     alignItems: 'center',
     justifyContent: 'center',
     marginBottom: 20,
@@ -80,6 +221,27 @@ const styles = StyleSheet.create({
   uploadIcon: {
     width: 100,
     height: 100,
+  },
+
+  uploadedPhoto: {
+    width: 100,
+    height: 100,
+    margin: 10,
+  },
+  photoContainer: {
+    position: 'relative',
+  },
+  deleteButton: {
+    position: 'absolute',
+    top: 3,
+    right: 3,
+    backgroundColor: '#ccc',
+    padding: 3,
+    borderRadius: 50,
+    alignItems: 'center',
+  },
+  createListingButton: {
+    marginBottom: 10, // Adjust the marginBottom value
   },
 });
 
