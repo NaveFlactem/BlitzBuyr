742d7990f8768f3fd63cb742453a5f6798bd6e0d
Luke <22577678+Iuke121@users.noreply.github.com>
2023-10-22 19:01:10 -0700

    Create Listing API endpoint

--- Backend/server.js
+++ Backend/server.js
@@ -3,34 +3,35 @@
  * @module Server
  */
 
-// Server setup
+// Required modules
 const express = require("express");
-const bodyParser = require("body-parser"); // You might need this later
 const app = express();
-app.use(express.json());
 const path = require("path");
 const db = require("./db");
 
-// Serve static files from the docs directory
-app.use("/docs", express.static(path.join(__dirname, "../docs")));
+// Middleware for parsing JSON request bodies
+app.use(express.json());
 
-/** GET request endpoint at the root which will load up the docs homepage
+/**
+ * GET request endpoint at the root which will redirect to the docs homepage.
  * @function
  * @name getRoot
- * @param {Object} req - Received express.js request object.
- * @param {Object} res - To be sent back express.js response object.
+ * @param {Object} req - Express.js request object.
+ * @param {Object} res - Express.js response object.
  */
 app.get("/", function (req, res) {
+  // Redirect to the docs homepage
   res.redirect("docs/index.html");
 });
 
 // #region Account Management API functions
 
-/** GET request endpoint at /accounts for retrieving a list of accounts.
+/**
+ * GET request endpoint at /accounts for retrieving a list of accounts.
  * @function
  * @name getAccounts
- * @param {Object} req - Received express.js request object.
- * @param {Object} res - To be sent back express.js response object.
+ * @param {Object} req - Express.js request object.
+ * @param {Object} res - Express.js response object.
  */
 app.get("/accounts", function (req, res) {
   // Query the database for a list of accounts
@@ -40,15 +41,17 @@ app.get("/accounts", function (req, res) {
       return res.status(500).json({ error: "Internal Server Error" });
     }
 
+    // Respond with a JSON array of accounts
     return res.status(200).json({ Accounts: rows });
   });
 });
 
-/** Handles login requests and performs authentication
+/**
+ * Handles login requests and performs authentication.
  * @name handleLoginRequest
  * @function
- * @param {Object} req - Received express.js request object.
- * @param {Object} res - To be sent back express.js response object.
+ * @param {Object} req - Express.js request object with a JSON body containing 'username' and 'password'.
+ * @param {Object} res - Express.js response object.
  * @example
  * // Sample HTTP POST request to '/login'
  * // Request body should contain a JSON object with 'username' and 'password' fields.
@@ -80,11 +83,12 @@ app.post("/login", function (req, res) {
   );
 });
 
-/** Handle user registration requests and account creation.
+/**
+ * Handles user registration requests and account creation.
  * @name handleRegistrationRequest
  * @function
- * @param {Object} req - Received express.js request object.
- * @param {Object} res - To be sent back express.js response object.
+ * @param {Object} req - Express.js request object with a JSON body containing 'username', 'password', 'confirmPassword', and 'email'.
+ * @param {Object} res - Express.js response object.
  * @example
  * // Sample HTTP POST request to '/register'
  * // Request body should contain a JSON object with 'username', 'password', 'confirmPassword', and 'email' fields.
@@ -93,11 +97,13 @@ app.post("/login", function (req, res) {
  */
 app.post("/register", function (req, res) {
   const { username, password, confirmPassword, email } = req.body;
-  // WIP DO THE CONFIRM PASSWORD CHECK (if they are equal)
-  if (confirmPassword !== password)
+
+  // Check if password and confirmPassword match
+  if (confirmPassword !== password) {
     return res
       .status(400)
       .json({ error: "Password and confirm password are not equal" });
+  }
 
   db.get(
     "SELECT Username FROM Accounts WHERE Username = ?",
@@ -120,7 +126,7 @@ app.post("/register", function (req, res) {
               console.error("Error registering the account:", err);
               return res.status(500).json({ error: "Internal Server Error" });
             }
-            // WIP: Consider adding constraints to the password (such as checking that it's strong enough with length, characters, etc.)
+            // WIP: Consider adding constraints to the password (such as checking that it's strong enough with length, characters, etc.).
             return res.status(201).json({
               message: "Account created",
               username: username,
@@ -133,12 +139,63 @@ app.post("/register", function (req, res) {
 });
 
 // #endregion
-/** Starts the Express server on a specified port and executes a callback function.
+
+// #region Listings Management API functions
+
+/**
+ * POST request endpoint at /createListing for creating a new listing.
  * @function
- * @name startServer
- * @param {number} port - The port at which the server will listen on.
- * @param {function} callback - The callback function called on server start.
+ * @name createListing
+ * @param {Object} req - Express.js request object with a JSON body containing 'price', 'images', 'title', 'description', and 'userName'.
+ * @param {Object} res - Express.js response object.
+ * @example
+ * // Sample HTTP POST request to '/createListing'
+ * // Request body should contain a JSON object with the required fields.
+ * const requestPayload = {
+ *   "price": 100.0,
+ *   "images": [image1_in_binary, image2_in_binary],
+ *   "title": "Sample Listing",
+ *   "description": "This is a sample listing.",
+ *   "userName": "sampleUser"
+ * };
  */
+app.post("/createListing", function (req, res) {
+  const { price, images, title, description, userName } = req.body;
+
+  const sqlTimeStamp = new Date().toISOString().slice(0, 19).replace("T", " ");
+
+  // Insert the listing into the Listings table
+  db.run(
+    "INSERT INTO Listings (price, title, description, userName, postDate) VALUES (?, ?, ?, ?, ?)",
+    [price, title, description, userName, sqlTimeStamp],
+    (err) => {
+      if (err) {
+        console.error("Error querying the database:", err);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+
+      // Insert images into the Images table
+      images.forEach((image) => {
+        db.run(
+          "INSERT INTO Images (imageId, listingId, imageData) VALUES (?, ?)",
+          [listingId, image],
+          (err) => {
+            if (err) {
+              console.error("Error querying the database:", err);
+              return res.status(500).json({ error: "Internal Server Error" });
+            }
+          }
+        );
+      });
+
+      return res.status(201).json({ message: "Listing created successfully" });
+    }
+  );
+});
+
+// #endregion
+
+// Start the Express server
 const server = app.listen(80, () => {
   const host = server.address().address;
   const port = server.address().port;
