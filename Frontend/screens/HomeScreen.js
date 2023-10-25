549e7542e8bcfa5837f629f2beb86d6c9ea31213
Luke <22577678+Iuke121@users.noreply.github.com>
2023-10-25 02:33:55 -0700

    Home page now shows images from listings

--- Frontend/screens/HomeScreen.js
+++ Frontend/screens/HomeScreen.js
@@ -1,95 +1,167 @@
-import React, {useEffect, useRef} from 'react';
-import { SafeAreaView, View, Image, StyleSheet, Dimensions, FlatList, Text, Button, TouchableOpacity} from 'react-native';
-import Swiper from 'react-native-swiper';
-import { useNavigation } from '@react-navigation/native';
-import BottomBar from '../components/BottomBar';
-import TopBar from '../components/TopBar';
+import React, { useEffect, useRef, useState } from "react";
+import { View, Image, StyleSheet, SafeAreaView, Text } from "react-native";
+import Swiper from "react-native-swiper";
+import BottomBar from "../components/BottomBar";
+import TopBar from "../components/TopBar";
+import Colors from "../constants/Colors";
 
 const styles = StyleSheet.create({
+  screenfield: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   container: {
     alignItems: "center",
     justifyContent: "center",
     zIndex: -1,
-    backgroundColor: '#D6447F',
+    backgroundColor: "#D6447F",
     height: "100%",
-    width: "100%",     
-  },
-  image: {
-    height: "8%",
-    width: "8%",
-    top: "1.5%",
+    width: "100%",
   },
   card: {
-    backgroundColor: '#F7A859',
+    backgroundColor: Colors.gray,
     width: "90%",
-    height: "70%",
+    height: "80%",
     borderRadius: 20,
     alignItems: "center",
     justifyContent: "center",
-    margin: 20,
+    margin: "5%",
     top: "9%",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 20,
+  },
+  titleContainer: {
+    position: "absolute",
+    bottom: 10, // Adjust the position as needed
+    left: 10, // Adjust the position as needed
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+    padding: 5,
+    borderRadius: 5,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "white", // Customize the color
+  },
+  price: {
+    fontSize: 14,
+    color: "white", // Customize the color
   },
 });
 
 const HomeScreen = () => {
-  const getListing = (listingId) => {
-    fetch(`blitzbuyr.lol/api/listing?id=${listingId}`, {
-      method: "GET",
-    }).then((response) => {
-      if (response.status == 200) {
-        // this means we got the listings successfully
-        return response.json(); // returns the listings
+  const [listings, setListings] = useState([]);
+  const [images, setImages] = useState([]);
+  const [swipeIndex, setSwipeIndex] = useState(0);
+  const didMount = useRef(false);
+
+  const fetchListings = async () => {
+    console.log("Fetching listings...");
+    try {
+      const listingsResponse = await fetch(
+        "http://blitzbuyr.lol/api/listings",
+        {
+          method: "GET",
+        }
+      );
+
+      if (listingsResponse.status <= 201) {
+        const listingsData = await listingsResponse.json();
+        setListings(listingsData.Listings);
+        setSwipeIndex(listingsData.Listings[0].ListingId);
       } else {
-        console.log(response.json()[message]);
+        console.log("Error fetching listings:", listingsResponse.status);
       }
-    });
+    } catch (err) {
+      console.log("Error:", err);
+    }
   };
 
-  const cardDictionary = {
-    0: ["red", "crimson", "tomato"],
-    1: ["orange", "darkorange", "orangered"],
-    2: ["yellow", "gold", "khaki"],
-    3: ["green", "limegreen", "forestgreen"],
-    4: ["blue", "dodgerblue", "deepskyblue"],
-    5: ["purple", "darkorchid", "blueviolet"],
-    6: ["pink", "deeppink", "palevioletred"],
-    7: ["brown", "saddlebrown", "chocolate"],
+  const fetchImages = async (index) => {
+    if (index == 0) return;
+    try {
+      //const id = listings[index].ListingId;
+      console.log("Fetching images for ListingID " + index);
+      const imagesResponse = await fetch(
+        `http://blitzbuyr.lol/api/images?listingId=${index}`,
+        {
+          method: "GET",
+        }
+      );
+
+      if (imagesResponse.status <= 201) {
+        const imagesData = await imagesResponse.json();
+        setImages(imagesData.Images);
+      } else {
+        console.log("Error fetching images:", imagesResponse.status);
+      }
+    } catch (err) {
+      console.log("Error:", err);
+    }
   };
-  const Listings = Object.entries(cardDictionary);
-  const navigation = useNavigation();
+
+  useEffect(() => {
+    fetchListings();
+  }, []);
+
+  useEffect(() => {
+    if (listings.length > 0) {
+      fetchImages(swipeIndex);
+    }
+  }, [swipeIndex, listings]);
 
   return (
-    
-    
     <SafeAreaView style={styles.screenfield}>
       <TopBar />
 
-      <View style={styles.container}>
-        <Swiper
-          showsButtons={false}
-          showsPagination={false}
-          vertical={true}
-          horizontal={false}
-        >
-          {Listings.map((listing, page) => (
-            <View style={styles.card} key={listing}>
-              <Swiper
-                horizontal={true}
-                showsButtons={false}
-                showsPagination={false}
-                loop={false}
-              >
-                {Listings[page].map((color, index) => (
-                  <View style={{backgroundColor: color, width: "90%", height: "70%", borderRadius: 20, alignItems: 'center', justifyContent: 'center', margin: 20, top: "9%"}} key={index}>
-                    <Text style={{fontSize: 30, fontWeight: 'bold', color: 'white'}}>{listing[0]}</Text>
-                  </View>
-                ))}
-              </Swiper>
-            </View>
-          ))}
-        </Swiper>
-      </View>
-      <BottomBar />            
+      {listings.length > 0 && (
+        <View style={styles.container}>
+          <Swiper
+            loop={false}
+            horizontal={false}
+            showsPagination={false}
+            showsButtons={false}
+            onIndexChanged={(index) => setSwipeIndex(listings[index].ListingId)}
+          >
+            {listings.map((item, listIndex) => {
+              return (
+                <View key={item.ListingId} style={styles.card}>
+                  <Swiper
+                    loop={false}
+                    horizontal={true}
+                    showsPagination={false}
+                    showsButtons={false}
+                  >
+                    {images.map((image, index) => {
+                      // console.log(`Listing ID: ${listIndex}, Image: blitzbuyr.lol/${image.ImageURI}`);
+                      return (
+                        <View key={index}>
+                          <Image
+                            style={styles.image}
+                            source={{
+                              uri: `http://blitzbuyr.lol/${image.ImageURI}`,
+                            }}
+                          />
+                          <View style={styles.titleContainer}>
+                            <Text style={styles.title}>{item.Title}</Text>
+                            <Text style={styles.price}>{item.Price}</Text>
+                          </View>
+                        </View>
+                      );
+                    })}
+                  </Swiper>
+                </View>
+              );
+            })}
+          </Swiper>
+        </View>
+      )}
+
+      <BottomBar />
     </SafeAreaView>
   );
 };
