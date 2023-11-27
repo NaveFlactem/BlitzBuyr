8090815b297704da1613470c961eb698756678f6
Evan Metcalf <evan.metcalf3@gmail.com>
2023-11-27 10:38:31 -0800

    fixing refresh animation

--- Frontend/components/visuals/CustomRefreshControl.js
+++ Frontend/components/visuals/CustomRefreshControl.js
@@ -7,13 +7,13 @@ import Animated, {
 import { screenHeight } from '../../constants/ScreenDimensions.js';
 import BouncePulse from './BouncePulse';
 
-export const CustomRefreshControl = memo(({ refreshing, scrollY }) => {
+export const CustomRefreshControl = memo(({ refreshing, scrollY, swiperRef }) => {
   const bouncePulseStyle = useAnimatedStyle(() => {
     let newYPosition = -100; // Initial off-screen position
     let newOpacity = 1;
 
     if (refreshing) {
-      newYPosition = withTiming(0, { duration: 300 }); // Bring into view when refreshing
+      newYPosition = withTiming(0, { duration: 500 });
     } else {
       // Normalize and adjust opacity based on scrollY
       const normalizedScrollY = Math.abs(scrollY.value); // Convert to positive value
@@ -38,7 +38,7 @@ const styles = StyleSheet.create({
   bouncePulseContainer: {
     position: 'absolute',
     alignSelf: 'center',
-    top: 0.08 * screenHeight,
+    top: 0,
     alignItems: 'center',
   },
 });
