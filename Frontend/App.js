75d226bab9db73078fafc5b0722b3ad3d47d45a4
ThomasPollicino <122580685+ThomasPollicino@users.noreply.github.com>
2023-12-06 20:11:52 -0800

    Suppress warnings

--- Frontend/App.js
+++ Frontend/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './nav/StackNavigator';
 import { CustomDarkTheme, CustomLightTheme } from './constants/Colors';
+import { LogBox } from 'react-native';
 import {
   ThemeProvider,
   useThemeContext,
 } from './components/visuals/ThemeProvider';
 
+
 const AppContainer = () => {
+  console.disableYellowBox=true;
+  LogBox.ignoreLogs(['Warning: ...']); //Hide warnings
+  LogBox.ignoreAllLogs();//Hide all warning notifications on front-end
   const { theme } = useThemeContext();
 
   return (
+    
     <NavigationContainer
       theme={theme === 'dark' ? CustomDarkTheme : CustomLightTheme}
     >
